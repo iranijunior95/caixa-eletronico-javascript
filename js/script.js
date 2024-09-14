d6fced4066732dc98df366bf44edc2b30055d55f
Irani junior <iranijunior95@gmail.com>
2024-09-14 17:19:47 -0300

    commit - capturar clique dos btns e identificar qual tecla foi clicada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 (function() {
+    const $btns = document.querySelectorAll('.btns');
     const $tbodyConteudoInfo = document.querySelector('#tbodyConteudoInfo');
+
+    $btns.forEach(btn => {
+        btn.addEventListener('click', function(event) {
+            if(event.target.innerHTML !== '') {
+
+                if(Number.isInteger(Number(event.target.innerText))) {
+                    console.log(event.target.innerText);
+                }
+                
+                if(event.target.innerText === 'enter' || event.target.innerText === 'cancel' || event.target.innerText === 'clean') {
+                    console.log(event.target.innerText);
+                }
+
+                if(event.target.innerText.match(/func/)) {
+                    console.log(event.target.innerText);
+                }
+            }
+        });
+    });
 
     function renderizarListaDadosContas() {
         const dadosContas = caixaEletronico.retornaListaDeContas();
